648295eb5e474626780a3b2a8f4e046402c461dd
kmd6225 <kdubartelL@gmail.com>
2023-08-31 21:47:12 -0600

    py function to write api response to csv

---
+++ python_scripts/mta_api.py
@@ -0,0 +1,20 @@
+import pandas as pd 
+import requests
+
+filter_date = '2023-01-01'
+def query_mta(fdate):
+    url = "https://data.ny.gov/resource/wujg-7c2s.json?$where=date_trunc_ym(transit_timestamp)='{}'".format(fdate)
+
+    payload = {}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    rjson = response.json()
+    try:
+        df = pd.DataFrame(rjson)
+        df.to_csv('C:\Users\kduba\OneDrive\Documents\MTA_DB\text_files\mta_{}.csv'.format(fdate))
+        return('success')
+    except:
+        return('failure')
+message = query_mta(filter_date)
+print(message)
